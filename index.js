f7074ce05969a0dd683c77a5dd43f869b9645284
kazimi.msu@gmail.com <kazimi.msu@gmail.com>
2023-04-10 16:48:41 +0400

    deleting

--- index.js
+++ index.js
@@ -11,22 +11,30 @@ const users = [];
 // Слушаем сообщения
 bot.on('message', (msg) => {
     // Если пользователь отправил "Привет"
-    if (msg.text.toLowerCase() === 'привет') {
+    if (msg.text.toLowerCase() === 'приффки') {
         // Отправляем ответное сообщение
         bot.sendMessage(msg.chat.id, 'ПрИфФкИ, ' + msg.from.first_name + '. КаК дЕлИфФкИ');
-        bot.sendMessage(msg.chat.id, 'id отправителя: ' + msg.from.id);
+    }
+
+    if (msg.text.toLowerCase() === 'привет') {
+        // Отправляем ответное сообщение
+        bot.sendMessage(msg.chat.id, 'Алейкум привет, ' + msg.from.first_name + '. Играть будем?');
     }
 
     if (msg.text.toLowerCase() === '+' || msg.text.toLowerCase() === 'плюс' || msg.text.toLowerCase() === 'plus') {
         if (!users.some(user => user.id === msg.from.id)) {
             users.push({id: msg.from.id, fullname: msg.from.first_name + ' ' + msg.from.last_name});
         }
-        bot.sendMessage(msg.chat.id, users.map((user, index) => (index+1) + ". " + user.fullname).join('\n'));
+        // bot.sendMessage(msg.chat.id, users.map((user, index) => (index+1) + ". " + user.fullname).join('\n'));
     }
 
     if (msg.text === '-' || msg.text.toLowerCase() === 'минус' || msg.text.toLowerCase() === 'minus') {
         bot.sendMessage(msg.chat.id, 'Это будет сложно, но как-нибудь выживем без тебя, ' + msg.from.first_name);
         users = users.filter(user => user.id !== msg.from.id);
+        // bot.sendMessage(msg.chat.id, users.map((user, index) => (index+1) + ". " + user.fullname).join('\n'));
+    }
+
+    if (msg.text.toLowerCase() === '\/список') {
         bot.sendMessage(msg.chat.id, users.map((user, index) => (index+1) + ". " + user.fullname).join('\n'));
     }
 
