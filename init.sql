ee979f61689807771f6607088c8da7fa15a7ba3a
kazimi.msu@gmail.com <kazimi.msu@gmail.com>
2023-04-16 10:56:11 +0400

    db restore

--- init.sql
+++ init.sql
@@ -9,24 +9,25 @@ CREATE TABLE users IF NOT EXISTS (
     user_id BIGINT UNIQUE NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT,
-    username TEXT
+    username TEXT,
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE chat_users IF NOT EXISTS (
+CREATE TABLE group_users IF NOT EXISTS (
     user_id BIGINT REFERENCES users(id),
     chat_id BIGINT NOT NULL,
     chat_role TEXT,
-    PRIMARY KEY(user_id)
+    PRIMARY KEY(user_id, chat_id)
 );
 
 CREATE TABLE games IF NOT EXISTS (
     id SERIAL PRIMARY KEY,
+    chat_id BIGINT NOT NULL,
     game_date DATE,
     game_starts TIME,
     game_ends TIME,
     place TEXT,
-    quote INT,
-    chat_id BIGINT NOT NULL,
+    participate_limit INT,
     status BOOLEAN,
     label TEXT
 );
@@ -37,4 +38,9 @@ CREATE TABLE game_users IF NOT EXISTS (
     participate_time TIMESTAMP,
     exactly BOOLEAN,
     PRIMARY KEY(user_id, game_id)
+);
+
+CREATE TABLE game_guest IF NOT EXISTS (
+    game_id BIGINT REFERENCES games(id),
+    fullname TEXT
 );
