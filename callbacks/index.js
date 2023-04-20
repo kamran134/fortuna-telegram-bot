0bd3ce812e302bdb06fcd0c9d655206c57d6fd84
kazimi.msu@gmail.com <kazimi.msu@gmail.com>
2023-04-20 10:11:28 +0400

    chatId

--- callbacks/index.js
+++ callbacks/index.js
@@ -54,13 +54,14 @@ function declineAppointment(pool, query, bot) {
 
 function deactiveGame(pool, query, bot) {
     const gameId = query.data.replace('deactivegame_', '');
+    const chatId = query.message.chat.id;
 
     pool.query(`UPDATE games SET status = FALSE WHERE id = $1`, [gameId])
         .then(res => {
             console.log(res);
             bot.sendMessage(chatId, 'Игра закрыта!');
         })
-        .catch(err => console.log('UPDATE ERROR___', err));
+        .catch(err => console.log('UPDATE GAMES ERROR', err));
 }
 
 module.exports = {
