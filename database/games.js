a2e9dad08d0650308fe7595abdfc1ae217e70507
Kamran Kazimi <kazimi.msu@gmail.com>
2023-04-22 23:21:10 +0400

    repair refactor 3

--- database/games.js
+++ database/games.js
@@ -4,7 +4,7 @@ async function addGame(pool, chatId, {date, start, end, quote, location, label})
     try {
         const result = await pool.query(`INSERT INTO games (game_date, game_starts, game_ends, quote, place, chat_id, status, ` +
             `label) VALUES ($1, $2, $3, $4, $5, $6, TRUE, $7) RETURNING id`, 
-            [moment(date, 'DD.MM.YYYY').toISOString(), start, end, quote, location, chatId, true, label]);
+            [moment(date, 'DD.MM.YYYY').toISOString(), start, end, quote, location, chatId, label]);
 
         console.log('insert result: ', JSON.stringify(result));
 
