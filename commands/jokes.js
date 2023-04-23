bd822997001c6be98b572db84c787062cca6520e
Kamran Kazimi <kazimi.msu@gmail.com>
2023-04-23 14:43:35 +0400

    seconds refactoring 8

--- commands/jokes.js
+++ commands/jokes.js
@@ -20,7 +20,7 @@ async function whatTime(msg, bot) {
         const gamesTimes = await getGamesTimesFromDatabase(chatId);
 
         if (gamesTimes && gamesTimes.length > 0) {
-            const gamesTimesString = gamesTimes.map(game => `${game.label}: ${moment(game.game_starts).format('hh:mm')}`).join(', ');
+            const gamesTimesString = gamesTimes.map(game => `${game.label}: ${moment(game.game_starts, 'HH:mm:ss').format('HH:mm')}`).join(', ');
             bot.sendMessage(chatId, `Мэээх. Сколько можно спрашивать? :/\n${gamesTimesString}`);
         }
     } catch (error) {
