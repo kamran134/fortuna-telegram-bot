714b8eef754c2404400362ab3cb1cc1e5d415ba6
Kamran Kazimi <kazimi.msu@gmail.com>
2023-04-29 08:29:45 +0400

    send long message

--- callbacks/users.js
+++ callbacks/users.js
@@ -1,10 +1,27 @@
 const { getUsersFromDatabase } = require("../database");
 
+async function sendMessage(bot, chatId, message) {
+    try {
+      const maxLength = 4096;
+      const messageLength = message.length;
+      const numMessages = Math.ceil(messageLength / maxLength);
+  
+      for (let i = 0; i < numMessages; i++) {
+        const start = i * maxLength;
+        const end = (i + 1) * maxLength;
+        const chunk = message.substring(start, end);
+  
+        await bot.sendMessage(chatId, chunk);
+      }
+    } catch (error) {
+      console.error('SEND MESSAGE ERROR: ', error);
+      throw error;
+    }
+}
+
 async function showUsersInSelectedGroup(query, bot) {
     const adminChatId = query.message.chat.id;
     const selectedGroupChatId = parseInt(query.data.split('_')[1]);
-
-    let gameDeactiveButtons = [];
 
     try {
         const users = await getUsersFromDatabase(selectedGroupChatId);
@@ -15,7 +32,7 @@ async function showUsersInSelectedGroup(query, bot) {
                 `На азербайджанском: ${user.fullname_az}`
             ).join('\n----------------------------------\n');
 
-            bot.sendMessage(adminChatId, usersString + "\n\nДля редактирования данных кого-то введите команду '/adminedituser [имя]/[фамилия]/[полное имя на азербайджанском]'");
+            await sendMessage(bot, adminChatId, usersString + "\n\nДля редактирования данных кого-то введите команду '/adminedituser [имя]/[фамилия]/[полное имя на азербайджанском]'");
         } else {
             bot.sendMessage(adminChatId, 'Ты не можешь редактировать игроков, если игроков игр нет');
         }
