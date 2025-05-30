d9cb6fd89c896f0df84dc1e243025f0fbf016015
Kamran Kazimi <kazimi.msu@gmail.com>
2025-05-31 00:36:50 +0400

    webhook test

--- index.js
+++ index.js
@@ -5,14 +5,33 @@ import { newChatMembers } from './events/newChatMembers.js';
 import { leftChatMember } from './events/leftChatMember.js';
 import { inlineQuery } from './events/inlineQuery.js';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 
 dotenv.config();
 
 // Устанавливаем токен, который вы получили от BotFather
 const token = process.env.TELEGRAM_TOKEN;
+const webhookUrl = 'https://42n.space:8443/bot' + token;
 
 // Создаем экземпляр бота
-const bot = new TelegramBot(token, { polling: true });
+//const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
+
+bot.setWebHook(webhookUrl, {
+    certificate: fs.readFileSync('/root/cert.pem'),
+});
+
+const server = https.createServer({
+    key: fs.readFileSync('/root/key.pem'),
+    cert: fs.readFileSync('/root/cert.pem'),
+}, (req, res) => {
+    bot.processUpdate(JSON.parse(req.body));
+    res.writeHead(200);
+    res.end();
+});
+
+server.listen(8443);
 
 bot.on('new_chat_members', async (msg) => {
     await newChatMembers(msg, bot);
